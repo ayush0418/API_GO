03d6815b332b3ded89180a553948f1247aac6c62
Ayush Bansal <xs372-ayuban@xs-host603-037.xs.neuralcompany.team>
2023-08-29 12:37:51 +0530

    Test Case updated for file attachment

--- TestCreateEmployee_test.go
+++ TestCreateEmployee_test.go
@@ -1,29 +1,52 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"bytes"
+	"mime/multipart"
+	"fmt"
 )
 
 func TestCreateEmployee(t *testing.T) {
 	r := SetUpRouter()
 	r.POST("/emp", createEmployee)
 
-	emp := employee{
-		Id:        7,
-		Name:      "xenonstack",
-		TeamName:  "CloudOps",
-		LeaveFrom: "2023-09-24",
-		LeaveTo:   "2023-09-25",
-		LeaveType: "Sick Leave",
-		Reporter:  "Sahil Bansal",
+	// Define form data
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	
+	// Add form fields
+	formFields := map[string]string{
+		"id":         "7",
+		"emp_name":   "xenonstack",
+		"team_name":  "CloudOps",
+		"leave_from": "2023-09-24",
+		"leave_to":   "2023-09-25",
+		"leave_type": "Sick Leave",
+		"reporter":   "Sahil Bansal",
 	}
 
-	jsonValue, _ := json.Marshal(emp)
-	req, _ := http.NewRequest("POST", "/emp", bytes.NewBuffer(jsonValue))
+	for key, val := range formFields {
+		_ = writer.WriteField(key, val)
+	}
+
+	// Add attachment
+	if formFields["leave_type"] == "Sick Leave" {
+		fmt.Println("Sick Leave")
+		fileContents := []byte("file attachment content")
+		part, _ := writer.CreateFormFile("attachment", "uploads/test.pdf")
+		part.Write(fileContents)
+	}
+
+	err := writer.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, _ := http.NewRequest("POST", "/emp", body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
 	w := httptest.NewRecorder()
 
 	r.ServeHTTP(w, req)
